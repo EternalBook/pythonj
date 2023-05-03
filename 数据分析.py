7a7d1417f7c5cd09eef59f1ddd3a6a8955f558e9
“EternalBook” <2435320866@qq.com>
2023-05-03 11:48:18 +0800

    Initial commit

--- 数据分析.py
+++ 数据分析.py
@@ -3,6 +3,13 @@ import pandas as pd
 
 def sales_import():
     data_csv = pd.read_csv(r'./Bakery sales.csv')
+    print(type(data_csv['unit_price']))
+    # data_csv["unit_price"] = data_csv["unit_price"].str.replace("€", "")
+    # data_csv["unit_price"] = data_csv["unit_price"].str.replace(",", ".")
+    # data_csv["unit_price"] = data_csv["unit_price"].str.replace(" ", "")
+    # data_csv["unit_price"] = data_csv["unit_price"].str.replace(r'\D', '').astype(float) / 100
+    data_csv["unit_price"] = data_csv["unit_price"].str.replace(r'\D', '', regex=True).astype(float) / 100
+    data_csv["sales volume"] = data_csv["Quantity"] * data_csv['unit_price']
     return data_csv
 
 
@@ -12,9 +19,26 @@ def popular_items(num):
     item_count = sales_import().groupby('article').count()['Quantity'].reset_index()
     item_count.sort_values(by='Quantity', ascending=False, inplace=True)
     hot_items = item_count.head(num)
-    hot_items.index = [i+1 for i in range(num)]  # 推导式 遍历1到num的行索引
+    hot_items.index = [i + 1 for i in range(num)]  # 推导式 遍历1到num的行索引
     return hot_items
 
 
+def hour_sales():
+    time_sales = sales_import()
+    # 使用lambda表达式将时间列转换为小时
+    time_sales['time'] = time_sales['time'].apply(lambda x: int(x.split(':')[0]))
+    # hourly_sales = time_sales.groupby('time')['article'].count()
+    hourly_sales = time_sales.groupby(['time', 'article'])['Quantity'].count().reset_index()
+    # print(hourly_sales)
+    
+    return hourly_sales
+    # time_sales = time_sales['time'].apply(lambda x: int(x.split(':')[0]))
+    # time_sales = time_sales.groupby('time')["Quantity"].count
+    # hourly_sales = time_sales.groupby(['time', 'article'])['Quantity'].count().reset_index()
+    # hourly_sales.index=[7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    # return hourly_sales
+
+
 if __name__ == "__main__":
-    print(popular_items(10))
+    hour_sales()
+    # print(sales_import())
--- 数据可视化.py
+++ 数据可视化.py
@@ -1,25 +1,33 @@
 from pyecharts import options as opts
 from pyecharts.charts import Bar
-from pyecharts.faker import Faker
 
 import 数据分析
 
 
-def popular_generate():
+# 生成前num销量的产品
+def popular_generate(num):
     popular_items = 数据分析.popular_items(10)
+    print(type(popular_items))
     c = (
         Bar()
-        .add_xaxis(popular_items['Quantity'])
-        .add_yaxis("商家A",Faker.values())
-        # .add_yaxis("商家B", Faker.values())
+        .add_xaxis(popular_items['article'].tolist())  # 传入数据需为list
+        .add_yaxis("销量", popular_items['Quantity'].tolist())
         .set_global_opts(
-            title_opts=opts.TitleOpts(title="Bar-显示 ToolBox"),
+            title_opts=opts.TitleOpts(title="销量前" + str(num) + "的商品"),
             toolbox_opts=opts.ToolboxOpts(),
             legend_opts=opts.LegendOpts(is_show=False),
+            xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=-25)),
         )
         .render("bar_toolbox.html")
     )
 
 
+def hourly_sales_generate():
+    hourly_sales = 数据分析.hour_sales()
+    print(hourly_sales)
+    # print(type(hourly_sales))
+
+
 if __name__ == "__main__":
-    popular_generate()
+    hourly_sales_generate()
+    # popular_generate(10)
